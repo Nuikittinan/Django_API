9bfeb5ac3f3ba25d5208ccf26b115e9bea833145
Nuikittinan <nhooyone11@gmail.com>
2022-01-19 17:13:06 +0700

    edit

--- sql/serializers.py
+++ sql/serializers.py
@@ -25,7 +25,7 @@ class DeviceSerializer(serializers.ModelSerializer):
 class TempSerializer(serializers.ModelSerializer):
     class Meta:
         model = DeviceAll
-        fields = ['hr','spo2','temp','create']
+        fields = ['hr','spo2','temp']
 
 # class Spo2Serializer(serializers.ModelSerializer):
 #     class Meta:
